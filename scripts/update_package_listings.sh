ca436bb8ba0b388530af68ed90bcbf26d8374303
Thomas Li <ailoodee@gmail.com>
2021-07-10 05:51:17 +0200

    update packages lists and script

--- scripts/update_package_listings.sh
+++ scripts/update_package_listings.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Saves explicitly installed packages (native/foreign) to text files.
 
-pacman -Qent > $HOME/.dotfiles/native-packages.txt
+pacman -Qentt > $HOME/.dotfiles/native-packages.txt
 pacman -Qm > $HOME/.dotfiles/foreign-packages.txt
 
