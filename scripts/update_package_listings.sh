08bd0083546a1e94b85f218e5e28a26ef6d1c85f
Thomas Li <ailoodee@gmail.com>
2021-09-05 02:49:03 +0200

    Remove version nums in package listings updating script

--- scripts/update_package_listings.sh
+++ scripts/update_package_listings.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Saves explicitly installed packages (native/foreign) to text files.
 
-pacman -Qentt > $HOME/.dotfiles/native-packages.txt
-pacman -Qm > $HOME/.dotfiles/foreign-packages.txt
+pacman -Qenq > $HOME/.dotfiles/native-packages.txt
+pacman -Qemq > $HOME/.dotfiles/foreign-packages.txt
 
